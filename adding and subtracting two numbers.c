bc9f32e48e0f0fa8178ee9f10fbb9208c61ac7c1
Mohammed Jubair M <126316744+mohammedjubair-14@users.noreply.github.com>
2023-03-22 21:16:24 +0530

    Add files via upload

---
+++ adding and subtracting two numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    printf("\n enter first value:");
+    scanf("%d",&a);
+    printf("\n enter the second value:");
+    scanf("%d",&b);
+    c=a+b;
+    d=a-b;
+    printf("\n addition of two numbers:%d",c);
+    printf("\n subtraction of two numbers:%d",d);
+    return 0;
+}
