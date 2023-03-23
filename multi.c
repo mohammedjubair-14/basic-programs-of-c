7fa9fb5ceafa0608c8dc9ee6d2c4a1c3ad488dcc
Mohammed Jubair M <126316744+mohammedjubair-14@users.noreply.github.com>
2023-03-23 22:22:03 +0530

    Add files via upload

---
+++ multi.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n,a,b,c;
+    printf("\n Enter the values :");
+    scanf("%d %d",&a,&b);
+    c=a*b;
+    printf("\n the multiplication of two numbers:%d",c);
+    return 0;
+}
+
