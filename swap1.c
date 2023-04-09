3b8575f48f197d3124a27485c7b1ff213898002d
Mohammed Jubair M <126316744+mohammedjubair-14@users.noreply.github.com>
2023-04-09 20:49:52 +0530

    Add files via upload

---
+++ swap1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int c,d,e;
+    printf("enter the first value:");
+    scanf("%d",&c);
+    printf("\n enter the second number:");
+    scanf("%d",&d);
+    printf("\n before swapping c is:%d",c);
+    printf("\n before swapping d is:%d",d);
+    c=e;
+    d=c;
+    d=e;
+    printf("\n after swapping c is:%d",c);
+    printf("\n after swapping d is:%d",d);
+    return 0;
+
